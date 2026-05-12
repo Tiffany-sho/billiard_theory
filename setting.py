072ec1c848979882d22437774076706251c5519b
Tiffany-sho <mituya1884tansan@gmail.com>
2026-05-12 13:09:23 +0900

    長方形型の関数作成

--- setting.py
+++ setting.py
@@ -2,7 +2,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 wall_width =4.0
+wall_heigth =2.0
 half_circle_diameter =2.0
+
+def squre_set(ax) :
+    ax.plot(0, 0)
+    ax.set_xlim(-wall_width /2 -1.0, wall_width /2 + 1.0)
+    ax.set_ylim(-wall_heigth /2 -1.0, wall_heigth /2 + 1.0)
+
+    wall_width_x = np.linspace(-wall_width /2 ,wall_width /2 ,100)
+    wall_heigth_y = np.linspace(-wall_heigth /2 ,wall_heigth /2 ,100)
+
+    plt.plot(wall_width_x, [wall_heigth/2 for _ in range(100)] , linewidth = 1,color='black')
+    plt.plot(wall_width_x, [-wall_heigth/2 for _ in range(100)] , linewidth = 1,color='black')
+    plt.plot([wall_width/2 for _ in range(100)] ,wall_heigth_y , linewidth = 1,color='black')
+    plt.plot([-wall_width/2 for _ in range(100)] ,wall_heigth_y , linewidth = 1,color='black')
 
 def stadium_set(ax) :
 
