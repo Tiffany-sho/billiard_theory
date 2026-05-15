e66e397d1b41b51170111d70b44e8807888678f3
Tiffany-sho <mituya1884tansan@gmail.com>
2026-05-15 12:18:52 +0900

    反射速度関数の設置

--- func/stadium/find_reflect_direction.py
+++ func/stadium/find_reflect_direction.py
@@ -1,19 +1,24 @@
 import numpy as np
 
-
 def find_reflect_direction(intersection,velocity,W) :
 
+    speed = np.linalg.norm(velocity)
+
     if np.abs(intersection[0]) <= W /2 :
-        return np.array([velocity[0] ,-velocity[1]])
-
-    if intersection[0] > W /2 :
-        center = np.array([W /2 , 0])
-    else :
-        center = np.array([-W /2 , 0])
+        reflected = np.array([velocity[0] ,-velocity[1]])
     
-    n = intersection - center
-    n_norm = np.linalg.norm(n)
+    else :
 
-    n_hat = n / n_norm
+        if intersection[0] > W /2 :
+            center = np.array([W /2 , 0])
+        else :
+            center = np.array([-W /2 , 0])
+        
+        n = intersection - center
+        n_norm = np.linalg.norm(n)
 
-    return velocity - 2 * np.dot(velocity,n_hat) * n_hat
+        n_hat = n / n_norm
+
+        reflected = velocity - 2 * np.dot(velocity,n_hat) * n_hat
+
+    return reflected / np.linalg.norm(reflected) * speed
