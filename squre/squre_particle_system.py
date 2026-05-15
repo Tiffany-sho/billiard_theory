902644e2de8c04b27c8a8e4fb373c8aecc1d9607
Tiffany-sho <mituya1884tansan@gmail.com>
2026-05-15 12:18:00 +0900

    スタジアムのセットアップ関数の追加

--- squre/squre_particle_system.py
+++ squre/squre_particle_system.py
@@ -2,31 +2,39 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
+from setting import wall_width, wall_heigth ,squre_set
+
 fig ,ax = plt.subplots()
-ax.plot(0, 0)
-position = np.array([0.0, 0.0])
-velocity = np.array([-0.05, 0.02])
+squre_set(ax)
 
-ax.set_xlim(-5, 5)
-ax.set_ylim(-5, 5)
+position = np.array([0.0,0.0])
+velocity = np.array([0.0,0.1])
 
-ax.plot([4, 4], [4, -4],color="black",linewidth = 1.0)
-ax.plot([4, -4], [-4, -4],color="black",linewidth = 1.0)
-ax.plot([-4, -4], [-4, 4],color="black",linewidth = 1.0)
-ax.plot([-4, 4], [4, 4],color="black",linewidth = 1.0)
+dot, = plt.plot([] ,[] , "o" , color = "black" ,ms = 3)
 
-dot, = plt.plot([], [], "o" , markersize=4.0)
+def init():
+    dot.set_data([], [])
+    return dot,
 
 def update(frame):
+
     position[0] += velocity[0]
     position[1] += velocity[1]
 
-    if np.abs(round(position[0],5)) == 4.00 :
+    if np.abs(round(position[0],5)) == wall_width /2 :
         velocity[0] *= -1
-    if np.abs(round(position[1],5)) == 4.00 :
+    if np.abs(round(position[1],5)) == wall_heigth /2 :
         velocity[1] *= -1
+
     dot.set_data([position[0]], [position[1]])
     return dot,
 
-ani = FuncAnimation(fig, update, frames=1000, interval=50 ,repeat=False,blit=True)
+ani = FuncAnimation(fig, update, frames=1000, init_func=init,interval=50 ,repeat=False,blit=True)
+
+ax.set_aspect('equal')
 plt.show()
