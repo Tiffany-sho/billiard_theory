09afe821ad631f7323ac0b97da0bfb9d46492a64
Tiffany-sho <mituya1884tansan@gmail.com>
2026-05-15 12:19:15 +0900

    交点を見つける関数の修正

--- func/stadium/find_intersection_reversion.py
+++ func/stadium/find_intersection_reversion.py
@@ -1,6 +1,16 @@
 import numpy as np
 
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+sys.path.append(os.path.join(os.path.dirname(__file__),'../calculate'))
+
+from newton_method import newton_method
+
 def find_intersection_reversion(point,velocity,W, D) :
+
+    newton_method_range = np.sqrt((W + D) ** 2 + D ** 2) / np.linalg.norm(velocity)
 
     print("パラメータ法実行")
 
@@ -9,13 +19,24 @@ def find_intersection_reversion(point,velocity,W, D) :
     
     if velocity[0] == 0 :
         x = point[0]
-        y = velocity[1] /np.abs(velocity[1]) * D / 2
-        return np.array([x,y])
+        if abs(point[0]) <= W/2:
+            y = np.sign(velocity[1]) * D / 2
+        elif point[0] < - W /2:
+            y = np.sign(velocity[1]) * np.sqrt((D /2) ** 2 - (x + W /2) ** 2)
+        else :
+            y = np.sign(velocity[1]) * np.sqrt((D /2) ** 2 - (x - W /2) ** 2)
 
+        return np.array([x,y])
+    
     if velocity[1] == 0 :
         y = point[1]
-        x = velocity[0] /np.abs(velocity[0]) * np.sqrt((D /2) ** 2 - y ** 2) + velocity[0] /np.abs(velocity[0]) * W / 2
+        if velocity[0] > 0 :
+            x = np.sign(y) * np.sqrt((D /2) ** 2 - y ** 2) + W / 2
+        else :
+            x = np.sign(y) * np.sqrt((D /2) ** 2 - y ** 2) - W / 2
+        print(f"交点x{np.array([x,y])}")
         return np.array([x,y])
+    
 
     temp_intersection_y = D /2 if velocity[1] > 0 else -D /2
     temp_intersection_t =( temp_intersection_y - point[1] ) / velocity[1]
@@ -34,12 +55,12 @@ def find_intersection_reversion(point,velocity,W, D) :
             
             right_t =np.inf
             if D_right >= 0 :
-                t1 = (- B_right - np.sqrt(D_right)) / A_right
-                t2 = (- B_right + np.sqrt(D_right)) / A_right
+                t1 =newton_method(A_right,2 * B_right,C_right,newton_method_range)
+                t2 =newton_method(A_right,2 * B_right,C_right,-newton_method_range)
 
                 valid_t = [t for t in (t1, t2) if t >= 0]
                 if valid_t:
-                    right_t = max(valid_t) 
+                    right_t = max(valid_t)
                     if point[0] + right_t * velocity[0] < W / 2:
                         right_t = np.inf
                     
@@ -53,14 +74,14 @@ def find_intersection_reversion(point,velocity,W, D) :
                 
             left_t =np.inf
             if D_left >= 0 :
-                t1 = (- B_left - np.sqrt(D_left)) / A_left
-                t2 = (- B_left + np.sqrt(D_left)) / A_left
+                t1 =newton_method(A_left,2 * B_left,C_left,newton_method_range)
+                t2 =newton_method(A_left,2 * B_left,C_left,-newton_method_range)
+
 
                 valid_t = [t for t in (t1, t2) if t >= 0]
                 if valid_t:
-                    left_t = max(valid_t) 
+                    left_t = max(valid_t)
                     if point[0] + left_t * velocity[0] > -W / 2:
-                        print(point[0] + left_t * velocity[0])
                         left_t = np.inf
-
+                    
                     return point + left_t * velocity
