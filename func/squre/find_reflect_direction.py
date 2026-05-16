7333fb12ad2e0bc56cc76b167a6b474ad7dcdda9
Tiffany-sho <mituya1884tansan@gmail.com>
2026-05-17 00:58:32 +0900

    長方形型の反射の関数修正

--- func/squre/find_reflect_direction.py
+++ func/squre/find_reflect_direction.py
@@ -1,8 +1,6 @@
 import numpy as np
 
 def find_reflect_direction(intersection,velocity,W,H) :
-    print(np.abs(intersection[0]) )
-    print(np.abs(intersection[1]) )
     speed = np.linalg.norm(velocity)
     if np.abs(intersection[0]) == W / 2 :
         reflected = np.array([-velocity[0] ,velocity[1]])
