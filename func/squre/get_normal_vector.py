5a55c428be01619ec2e92acc77918e9b86ece84f
Tiffany-sho <mituya1884tansan@gmail.com>
2026-05-19 15:43:43 +0900

    単位ベクトル関数作成

---
+++ func/squre/get_normal_vector.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def get_normal_vector(intersection ,v ,wall_width ,wall_height):
+    if np.isclose(abs(intersection[0]), wall_width /2) and np.isclose(abs(intersection[1]), wall_height /2):
+        n = np.array([-v[0] / np.linalg.norm(v) ,-v[1] / np.linalg.norm(v)])
+    elif np.isclose(abs(intersection[0]), wall_width /2):
+        n = np.array([-np.sign(intersection[0]) ,0])
+    else:
+        n = np.array([0 ,-np.sign(intersection[1])])
+
+    return n
--- squre/squre_poincare_map.py
+++ squre/squre_poincare_map.py
@@ -12,6 +12,7 @@ sys.path.append(os.path.join(os.path.dirname(__file__),'../func/squre'))
 from setting import wall_width, wall_height ,poincare_map_set
 from find_intersection_reversion import find_intersection_reversion
 from find_reflect_direction import find_reflect_direction
+from get_normal_vector import get_normal_vector
 
 fig ,ax = plt.subplots()
 poincare_map_set(ax)
@@ -34,19 +35,10 @@ def create_poincare_dot(initial_position ,initial_velocity):
 
         set_collision_angle = np.arctan2(intersection[1] ,intersection[0])
 
-        if np.isclose(abs(intersection[0]), wall_width /2) and np.isclose(abs(intersection[1]), wall_height /2):
-            print("==========================")
-            n = np.array([-v[0] / np.linalg.norm(v) ,-v[1] / np.linalg.norm(v)])
-        elif np.isclose(abs(intersection[0]), wall_width /2):
-            n = np.array([-np.sign(intersection[0]) ,0])
-        else:
-            n = np.array([0 ,-np.sign(intersection[1])])
+        n = get_normal_vector(intersection,v,wall_width,wall_height)
         
         set_reflection_sin = np.cross(v /np.linalg.norm(v) ,n)
-        print(v)
-        print(n)
-        print(set_reflection_sin)
-
+       
         p = intersection
         v = reflected_v
         collision_angle.append(set_collision_angle)
