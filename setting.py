bbaa6946b042749e7ec34a55d4177479acd5a36d
Tiffany-sho <mituya1884tansan@gmail.com>
2026-05-25 17:44:52 +0900

    保存にする変更

--- setting.py
+++ setting.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.ticker as ticker
 import matplotlib.pyplot as plt
 
-wall_width =100.0
+wall_width =1.0
 wall_height =1.0
 
 half_circle_diameter =1.0
@@ -10,8 +10,8 @@ half_circle_diameter =1.0
 sinai_wall_height = 2.0
 sinai_circle_diameter = 1.0
 
-initial_position = np.array([0.0 ,0.5])
-initial_velocity = np.array([-0.03, -0.0001])
+initial_position = np.array([0.0 ,0.4])
+initial_velocity = np.array([0.01, 0.01])
 
 def squre_set(ax) :
     ax.plot(0, 0)
@@ -61,10 +61,21 @@ def sinai_set(ax) :
     plt.plot(sinai_circle_x ,sinai_up_circle_y , linewidth = 1,color='black')
     plt.plot(sinai_circle_x,sinai_down_circle_y , linewidth = 1,color='black')
 
-def poincare_map_set(ax):
+def poincare_map_set(ax,W,H):
+
+    ax.set_title(f"W = {W},H = {H}")
+
     ax.plot(0, 0)
     ax.set_xlim(-np.acos(-1), np.acos(-1))
-    ax.set_ylim(-1 , 1 )
+
+    ax.axvline(x=np.atan2(H,W),color = "red" ,linestyle="--",label = "atan(W/H)")
+    ax.axvline(x=-np.atan2(H,W),color = "red" ,linestyle="--",label = "-atan(W/H)")
+    ax.axvline(x=np.atan2(H,W) - np.pi,color = "red" ,linestyle="--",label = "atan(W/H)")
+    ax.axvline(x=-np.atan2(H,W) + np.pi,color = "red" ,linestyle="--",label = "-atan(W/H)")
 
     ax.set_xticks([-np.pi,-3*np.pi/4, -np.pi/2,-np.pi/4, 0,np.pi/4, np.pi/2,3*np.pi/4, np.pi])
     ax.set_xticklabels([r'$-\pi$',r'$-3\pi/4$', r'$-\pi/2$',r'$-\pi/4$', r'$0$',r'$\pi/4$', r'$\pi/2$',r'$-3\pi/4$', r'$\pi$'])
+
+def poincare_map_arc_set(ax):
+    ax.plot(0, 0)
+    ax.set_xlim(-(wall_width + half_circle_diameter * np.pi / 2), wall_width + half_circle_diameter * np.pi / 2)
