27d309b655c7bd17998763c1778b432e32daeaac
Tiffany-sho <mituya1884tansan@gmail.com>
2026-05-25 14:43:32 +0900

    ヤコビアン生成関数

---
+++ func/stadium/get_jacobian.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+def get_jacobian(intersection ,W ,H ,angle):
+
+    if np.abs(intersection[0]) <= W /2:
+        r = H / 2 / np.sin(angle)
+        dr_dsita = - H / 2 * np.cos(angle) / np.sin(angle) ** 2
+
+        print(np.sqrt(r ** 2 + dr_dsita ** 2))
+        print(H / 2 / np.sin(angle) ** 2)
+
+    elif intersection[0] > W / 2 :
+        D = (H / 2) ** 2 - (W / 2 * np.sin(angle)) ** 2
+        r = W / 2 * np.cos(angle) + np.sqrt(D)
+        dr_dsita = - W / 2 * np.sin(angle) - (W / 2) ** 2 * np.sin(angle) * np.cos(angle) / np.sqrt(D)
+    
+    else :
+        D = (H / 2) ** 2 - (W / 2 * np.sin(angle)) ** 2
+        r = - W / 2 * np.cos(angle) + np.sqrt(D)
+        dr_dsita = W / 2 * np.sin(angle) - (W / 2) ** 2 * np.sin(angle) * np.cos(angle) / np.sqrt(D)
+
+    jacobian = np.sqrt(r ** 2 + dr_dsita ** 2)
+    
+
+    return jacobian
+
--- func/stadium/get_normal_vector.py
+++ func/stadium/get_normal_vector.py
@@ -1,14 +1,14 @@
 import numpy as np
 
-def get_normal_vector(intersection ,wall_width ,wall_height):
+def get_normal_vector(intersection ,W ,H):
 
-    if abs(intersection[1]) == wall_height /2:
+    if abs(intersection[1]) == H /2:
         n = np.array([0 ,-np.sign(intersection[1])])
     else:
-        if intersection[0] > wall_width /2 :
-            center = np.array([wall_width /2 , 0])
+        if intersection[0] > W /2 :
+            center = np.array([W /2 , 0])
         else :
-            center = np.array([-wall_width /2 , 0])
+            center = np.array([-W /2 , 0])
         
         n = intersection - center
         n_norm = np.linalg.norm(n)
