66325e9dbec3606a7a299cac957bb75e31239d59
Tiffany-sho <mituya1884tansan@gmail.com>
2026-05-25 17:21:56 +0900

    誤差の判定を甘くした

--- func/calculate/newton_method.py
+++ func/calculate/newton_method.py
@@ -1,9 +1,11 @@
 import numpy as np
 
 
-esp = 1.0e-14
+esp = 1.0e-10
 
 def newton_method (a ,b ,c ,range):
+
+    print("ニュートン法実行")
 
     d = b * b -4 * a * c
     if d >= 0 :
@@ -15,6 +17,6 @@ def newton_method (a ,b ,c ,range):
             new_x = (a * x ** 2 - c) /(2 * a * x + b )
             x = new_x
             error = a * x ** 2 + b * x + c
-
+        print("ニュートン法実行終了")  
         return x
  
