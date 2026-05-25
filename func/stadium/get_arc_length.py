cce1f4954bf40af457bbb4d05bd9f572fd3d21b6
Tiffany-sho <mituya1884tansan@gmail.com>
2026-05-25 17:23:17 +0900

    弧長の関数の作成

---
+++ func/stadium/get_arc_length.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+def get_arc_length(intersection ,W ,H):
+
+    if abs(intersection[0]) <= W / 2:
+        if intersection[1] > 0:
+            return intersection[0]
+        else:
+            if intersection[0] > 0 :
+                return  W / 2 + H / 2 * np.pi + intersection[0]
+            else:
+                return  -W / 2 - H / 2 * np.pi + intersection[0]
+
+    else :
+        arc = np.acos(2 * intersection[1] / H) * H / 2
+        if intersection[0] > 0 :
+            return W / 2 + arc
+        else:
+            return - W / 2 -arc
