76a31b43b6fdbc4c6f97e87d4fabdce2c22210c2
Tiffany-sho <mituya1884tansan@gmail.com>
2026-05-26 14:10:29 +0900

    ポアンカレ断面と軌道の同時表示

--- squre/squre_liner_system.py
+++ squre/squre_liner_system.py
@@ -8,44 +8,46 @@ import os
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 sys.path.append(os.path.join(os.path.dirname(__file__),'../func/squre'))
 
-from setting import wall_width, wall_height ,initial_position ,initial_velocity,squre_set
+from setting import squre_set
 from find_intersection_reversion import find_intersection_reversion
 from find_reflect_direction import find_reflect_direction
 
-fig ,ax = plt.subplots()
-squre_set(ax)
+def squre_line_system(initial_position,initial_velocity,W,H):
 
-print("-------------------------------------")
-print(f"初期値:{initial_position}")
-print(f"初速度:{initial_velocity}")
-
-position = initial_position.copy()
-velocity = initial_velocity.copy()
-
-def update(frame):
-
-    intersection = find_intersection_reversion(position,velocity,wall_width, wall_height)
+    fig ,ax = plt.subplots()
+    squre_set(ax,W,H)
 
     print("-------------------------------------")
-    print(f"位置:{position}")
-    print(f"速度:{velocity}")
-    print(f"速度の大きさ:{np.linalg.norm(velocity)}")
-    print(f"y = {velocity[1] /velocity[0]}x + {-velocity[1] /velocity[0] * position[0] + position[1]}")
-    print(f"交点:{intersection}")
-    plt.plot([intersection[0]],[intersection[1]] , "o",color = "black" ,ms = 3)
+    print(f"初期値:{initial_position}")
+    print(f"初速度:{initial_velocity}")
 
-    ordit_x = np.linspace(intersection[0] ,position[0] ,100) if intersection[0] - position[0] != 0 else [position[0] for _ in range(100)]
-    ordit_y = velocity[1] /velocity[0] * (ordit_x - position[0]) + position[1] if velocity[0] != 0 else np.linspace(intersection[1] ,position[1] ,100)
+    position = initial_position.copy()
+    velocity = initial_velocity.copy()
 
-    plt.plot(ordit_x,ordit_y,color = "black" ,linewidth = 1 ,alpha=0.1)
+    def update(frame):
 
-    position[:2] = intersection[:2]
-    reflected_velocity = find_reflect_direction(position,velocity,wall_width,wall_height)
-    print(f"反射速度:{reflected_velocity}")
-    velocity[:2] = reflected_velocity[:2]
+        intersection = find_intersection_reversion(position,velocity,W, H)
+
+        print("-------------------------------------")
+        print(f"位置:{position}")
+        print(f"速度:{velocity}")
+        print(f"速度の大きさ:{np.linalg.norm(velocity)}")
+        print(f"y = {velocity[1] /velocity[0]}x + {-velocity[1] /velocity[0] * position[0] + position[1]}")
+        print(f"交点:{intersection}")
+        plt.plot([intersection[0]],[intersection[1]] , "o",color = "black" ,ms = 3)
+
+        ordit_x = np.linspace(intersection[0] ,position[0] ,100) if intersection[0] - position[0] != 0 else [position[0] for _ in range(100)]
+        ordit_y = velocity[1] /velocity[0] * (ordit_x - position[0]) + position[1] if velocity[0] != 0 else np.linspace(intersection[1] ,position[1] ,100)
+
+        plt.plot(ordit_x,ordit_y,color = "black" ,linewidth = 1 ,alpha=0.1)
+
+        position[:2] = intersection[:2]
+        reflected_velocity = find_reflect_direction(position,velocity,W,H)
+        print(f"反射速度:{reflected_velocity}")
+        velocity[:2] = reflected_velocity[:2]
 
 
-ani = FuncAnimation(fig, update, frames=100, interval=100 ,repeat=False)
+    ani = FuncAnimation(fig, update, frames=100, interval=100 ,repeat=False)
 
-ax.set_aspect('equal')
-plt.show()
+    ax.set_aspect('equal')
+    plt.show()
--- squre/squre_poincare_map.py
+++ squre/squre_poincare_map.py
@@ -9,20 +9,24 @@ import os
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 sys.path.append(os.path.join(os.path.dirname(__file__),'../func/squre'))
 
-from setting import wall_width, wall_height ,poincare_map_set
+from setting import squrt_poincare_map_arc_set
+from squre_liner_system import squre_line_system
 from find_intersection_reversion import find_intersection_reversion
 from find_reflect_direction import find_reflect_direction
 from get_normal_vector import get_normal_vector
+# from get_jacobian import get_jacobian
+from get_arc_length import get_arc_length
 
-
+wall_width =1.0
+wall_height =2.0
 
 position_1 = np.array([0.0,0.5])
-velocity_1 = np.array([-0.03, -0.05])
+velocity_1 = np.array([-0.031, -0.051])
 
 def create_poincare_dot(initial_position ,initial_velocity ,W ,H):
 
     fig ,ax = plt.subplots()
-    poincare_map_set(ax ,W ,H)
+    squrt_poincare_map_arc_set(ax ,W ,H)
 
     collision_angle = []
     reflection_sin = []
@@ -31,22 +35,24 @@ def create_poincare_dot(initial_position ,initial_velocity ,W ,H):
     v = initial_velocity.copy()
 
     for _ in range(50):
-        intersection = find_intersection_reversion(p ,v ,wall_width ,wall_height)
-        reflected_v = find_reflect_direction(intersection ,v ,wall_width,wall_height)
+        intersection = find_intersection_reversion(p ,v ,W ,H)
+        reflected_v = find_reflect_direction(intersection ,v ,W,H)
 
-        set_collision_angle = np.arctan2(intersection[1] ,intersection[0])
+        set_arc_length = get_arc_length(intersection,W,H)
 
-        n = get_normal_vector(intersection,v,wall_width,wall_height)
+        n = get_normal_vector(intersection,v,W,H)
+
+        # jacobian = get_jacobian(intersection,W,H,set_collision_angle)
         
-        set_reflection_sin = np.cross(v /np.linalg.norm(v) ,n)
-       
+        set_reflection_sin = np.cross(v /np.linalg.norm(v) ,n) 
         p = intersection
         v = reflected_v
-        collision_angle.append(set_collision_angle)
+        collision_angle.append(set_arc_length)
         reflection_sin.append(set_reflection_sin)
 
     plt.scatter(collision_angle,reflection_sin)
 
-create_poincare_dot(position_1,velocity_1 ,1 ,1)
+create_poincare_dot(position_1,velocity_1 ,wall_width ,wall_height)
+squre_line_system(position_1,velocity_1 ,wall_width ,wall_height)
 
 plt.show()
